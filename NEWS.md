5aa165f21b92dde2a079c9f3212002702a93817b
James J Balamuta <coatless@users.noreply.github.com>
2020-01-05 09:35:35 -0600

    Refresh ghapi3 package (#8)
    * Update package level documentation
    
    * Update DESCRIPTION file
    
    * Re-build documentation
    
    * Remove TravisCI
    
    * Add R CMD Check
    
    * Add GitHub Actions
    
    * Update README format

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# ghapi3 0.0.1
+
+## Deployment
+
+- CI is done using GitHub Actions.
--- R/ghapi3-package.R
+++ R/ghapi3-package.R
@@ -1,3 +1,10 @@
+#' @keywords internal
 #' @details
 #' This package requires users have access to a GitHub Personal Access Token (PAT)
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
