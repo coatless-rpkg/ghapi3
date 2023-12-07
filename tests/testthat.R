27ea9b1321404182334196730115cac7b139684a
James J Balamuta <james.balamuta@gmail.com>
2023-12-07 02:09:46 -0800

    Remove tests until vcr update

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(ghapi3)
-
-test_check("ghapi3")
--- tests/testthat/test-organization.R
+++
@@ -1,7 +0,0 @@
-context("organization")
-
-test_that("check membership", {
-
-  ## Add test
-  expect_equal(2L+2L, 4L)
-})
